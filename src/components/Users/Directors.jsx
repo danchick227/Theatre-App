844f35e8055a3939d0bec501bb83187f5f838703
maria <pineginamaria02.com@gmail.com>
2025-10-30 15:10:15 +0500

    добавили режиссёров

---
+++ src/components/Users/Directors.jsx
@@ -0,0 +1,22 @@
+import "./Artists.css";
+import { directors } from "../../Data/directors.js";
+
+export default function Directors() {
+  return (
+    <div className="artists-page">
+      <h2>Режисёры театра</h2>
+      <div className="artists-grid">
+        {artists.map((directors) => (
+          <div className="artist-card" key={directors.id}>
+            <img src={directors.image} alt={directors.name} />
+            <div className="artist-info">
+              <h3>{directors.name}</h3>
+              <p className="role">{directors.role}</p>
+              <p className="exp">Стаж: {directors.experience}</p>
+            </div> 
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
