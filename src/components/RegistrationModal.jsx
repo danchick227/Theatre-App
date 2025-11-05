628ef3cb4f28547dc8dadb96aaee1379e96689cf
Данил <saydakov09@gmail.com>
2025-11-05 23:48:14 +0500

    добавил форму регистрации

---
+++ src/components/RegistrationModal.jsx
@@ -0,0 +1,24 @@
+import "./LoginModal.css";
+
+export default function RegistrationModal({ onClose, onSuccess }) {
+  return (
+    <div className="modal-overlay" onClick={onClose}>
+      <div className="modal" onClick={(e) => e.stopPropagation()}>
+        <h2>Регистрация / Вход</h2>
+
+        <form>
+          <input type="text" placeholder="Логин" required />
+          <input type="password" placeholder="Пароль" required />
+          <input type="password" placeholder="Пароль" required />
+          <button type="submit" onClick={onSuccess}>
+            Войти
+          </button>
+        </form>
+
+        <button className="close-btn" onClick={onClose}>
+          ✕
+        </button>
+      </div>
+    </div>
+  );
+}
