95194fcfb65a964c2c289801a869a4438a5db196
Данил <saydakov09@gmail.com>
2025-11-05 23:47:51 +0500

    метод авторизации

---
+++ src/api/authApi.js
@@ -0,0 +1,35 @@
+const API_URL = "https://localhost:7078/api";
+
+export async function login(login, password) {
+  const response = await fetch(`${API_URL}/auth/login`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ login, password }),
+  });
+
+  const data = await response.json();
+
+  if (!response.ok) {
+    throw new Error(data.message || "Ошибка авторизации");
+  }
+
+  return data;
+}
+
+export async function register(login, password) {
+  const response = await fetch(`${API_URL}/auth/register`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ login, password }),
+  });
+
+  const data = await response.json();
+
+  if (!response.ok) {
+    throw new Error(data.message || "Ошибка регистрации");
+  }
+
+  return data;
+}
